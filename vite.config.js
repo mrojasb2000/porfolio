4c43ab26697f14462d7a42f835480b1d0be89a63
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-03-01 18:56:07 -0300

    refactor: add vitest/coverage-v8 dependecy

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.js',
+      ]
+    }
   }
 })
