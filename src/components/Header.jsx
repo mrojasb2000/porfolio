3fc48be681ff8bb55bc01937427017808af76a58
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-03-01 21:09:52 -0300

    refactor: components structures

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header({ logo }) {
+  return (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </header>
+  );
+}
+
+export default Header;
